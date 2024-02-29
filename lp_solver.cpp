d19e75f1dd701da3bc9ec028ab2dc35bc0f15ffc
Guadadada <chao.zhang.1@stonybrook.edu>
2024-02-29 16:18:48 -0500

    finish solve_1d code structure

--- lp_solver.cpp
+++ lp_solver.cpp
@@ -8,7 +8,38 @@ LPSolver::LPSolver(Initializer *init, Global_Data *g, ParticleViewer *v) {
 }
 
 void LPSolver::solve_1d(){
-  
+
+    while(currenttime < tend)
+    {
+        // generate boundary particles
+
+        // reorder particles, make sure particle position is in order
+
+        // compute the cfl dt
+        computeCFLCondition();
+        // adjust dt by write time interval
+        bool iswritestep = adjustDtByWriteTimeInterval(); 
+
+        // heating model
+        
+        // lax-wendroff solver
+        solve_laxwendroff();
+
+        // compute boundary condition
+
+        // update particle states
+
+        // radiation cooling
+
+        // move particles
+
+        // coupute temperature
+
+        // output data
+
+        // delete remote particles
+        
+    }
 
 
 
