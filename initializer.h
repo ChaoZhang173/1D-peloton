1574a2eee14cdd0cbe24f1ed59de5ccd026b2d5d
Chao Zhang <88896780+ChaoZhang173@users.noreply.github.com>
2024-02-28 20:48:36 -0500

    Create initializer.h

---
+++ initializer.h
@@ -0,0 +1,25 @@
+#ifndef __INITIALIZER_H__
+#define __INITIALIZER_H__
+
+using namespace std;
+class Global_Data;
+
+/*
+This file is for initializer class. 
+It is used to initialize the parameters and pass them to Global_Data class.
+In later version may use a input file to initialize, but currently only set to default values.
+
+*/
+
+class Initializer {
+friend class Global_Data; // Global_Data has access to private and proteced members
+public:
+// constructor
+Initializer();
+
+private:
+// currently not using
+void readInputfile(const std::string& inputfileName);
+void setParameters();
+
+}
