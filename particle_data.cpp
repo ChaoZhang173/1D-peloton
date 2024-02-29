639e70a258fc1a1ae4278c49a9cb8b0fab37b90e
Chao Zhang <88896780+ChaoZhang173@users.noreply.github.com>
2024-02-28 21:33:23 -0500

    Create particle_data.cpp

---
+++ particle_data.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <time.h>
+#include <math.h>
+#include <algorithm>
+#include "particle_data.h"
+
+using namespace std;
+
+Global_Data::Global_Data(Initializer *init) {
+
+
+}
