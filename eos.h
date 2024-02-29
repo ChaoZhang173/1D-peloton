49397f73cf357af838083b4d2a244447954f9bf8
Chao Zhang <88896780+ChaoZhang173@users.noreply.github.com>
2024-02-29 10:05:46 -0500

    Create eos.h

---
+++ eos.h
@@ -0,0 +1,14 @@
+#ifndef __EOS_H__
+#define __EOS_H__
+
+/*
+This file is for equation of state class. 
+It has the following eos:
+  1. polytropic (currently working)
+  2. saha neon (in the future)
+  3. others
+*/
+
+
+
+#endif
