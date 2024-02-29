5156b736d0f668fab2fe0868bd5a09339622b396
Chao Zhang <88896780+ChaoZhang173@users.noreply.github.com>
2024-02-28 21:39:32 -0500

    Create particle_viewer.h

---
+++ particle_viewer.h
@@ -0,0 +1,22 @@
+#ifndef __PARTICLE_VIEWER_H__
+#define __PARTICLE_VIEWER_H__
+
+#include "particle_data.h"
+
+/*
+This file is used to output data.
+features:
+1. output data file
+2. print plot using matlab/pyton
+*/
+
+class ParticleViewer {
+public:
+ParticleViewer(Global_Data * data);
+
+~ParticleViewer(){}
+
+void writeResult(int step,double time);
+
+};
+#endif
