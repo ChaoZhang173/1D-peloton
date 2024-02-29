4424bccab6cb9bc9a46f0b2f5db5a96b973befe9
Chao Zhang <88896780+ChaoZhang173@users.noreply.github.com>
2024-02-28 20:27:46 -0500

    Create peloton_main.cpp
    main function of 1d peloton code

---
+++ peloton_main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include <sys/stat.h> // mkdir()
+#include <cstring> // strerror()
+
+#include "particle_data.h"
+#include "lp_solver.h"
+#include "particle_viewer.h"
+
+using namespace std;
+
+
+int main(){
+  //initialize the files
+
+  
+  // initialize the initializer  may use input file in later developement
+  Initializer *init = new Initializer();
+
+  // use initializer to initialize the global data
+  Global_Data *gdata = new Global_Data(init);
+
+  // initialize the viewer part, used to output data
+  ParticleViewer *viewer = new ParticleViewer(gdata,outputfileNameFluid);
+
+  // initialize the lp solver
+  LPSolver *lpsolver = new LPSolver(init, gdata, viewer);
+
+  // initialize the fluid particles, may add using restart later
+  gdata->initFluidParticles_line();
+
+  // solve the pde
+  lpsolver->solver_1d();
+
+  // clean the data
+  gdata->cleanUpArrays();
+
+  // clean the objects
+  delete init;
+  delete gdata;
+  delete lpsolver;
+  
+  return 0;
+}
