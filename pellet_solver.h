990132b7cdc82b304070aa1d78de12c50d8c96c6
Chao Zhang <88896780+ChaoZhang173@users.noreply.github.com>
2024-02-28 21:59:45 -0500

    Create pellet_solver.h

---
+++ pellet_solver.h
@@ -0,0 +1,29 @@
+#ifndef __PELLET_SOLVER_H__
+#define __PELLET_SOLVER_H__
+
+#include "particle_data.h"
+#include "material_lib.h"
+#include "initializer.h"
+
+/*
+This file is for pellet solver.
+The class PelletSolver stores all functions that related to pellet problem.
+*/
+
+class PelletSolver{
+public:
+  PelletSolver (Initializer* init,Global_Data *gdata);
+  ~PelletSolver();
+
+  void heatingModel(double dt);
+
+  double mu;
+  double mass;
+  double Z;
+  double I;
+  double sublimationenergy;
+  double teinf;
+  double neinf;
+  double neeff;
+};
+#endif
