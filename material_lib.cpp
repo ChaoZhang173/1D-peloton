6aa5ad26f869fe11c895a44a2ce53e6f3091a595
Chao Zhang <88896780+ChaoZhang173@users.noreply.github.com>
2024-03-03 13:16:00 -0500

    Create material_lib.cpp

---
+++ material_lib.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "material_lib.h"
+
+using namespace std;
+
+Neon::Neon(){
+    mu = 20.1797; 
+    mass = 3.351e-23;
+    Z = 10;
+    I = 135.5;
+    sublimationEnergy = 1363;
+}
+
+// to be finished
+double Neon::getOne_Plus_Zstar(double teinf){
+
+}
