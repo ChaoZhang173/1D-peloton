39e9c4be377ee130eafbbfc539f5946febe088d5
Chao Zhang <88896780+ChaoZhang173@users.noreply.github.com>
2024-03-18 14:58:17 -0400

    Create register.cpp

---
+++ register.cpp
@@ -0,0 +1,9 @@
+#include "registrar.h"
+#include "state_pellet.h"
+#include "boundary_pellet.h"
+namespace{
+
+
+    StateRegistrar<PelletState> s1("pelletstate");
+    BoundaryRegistrar<PelletInflowBoundary> b1("pelletinflowboundary");
+}
