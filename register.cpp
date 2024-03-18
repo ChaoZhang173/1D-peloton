fd34112ee933960c01e40b3cd6dad517343c4834
Guadadada <chao.zhang.1@stonybrook.edu>
2024-03-18 15:02:15 -0400

    update register

--- register.cpp
+++ register.cpp
@@ -1,8 +1,7 @@
-#include "registrar.h"
+#include "register.h"
 #include "state_pellet.h"
 #include "boundary_pellet.h"
 namespace{
-
 
     StateRegistrar<PelletState> s1("pelletstate");
     BoundaryRegistrar<PelletInflowBoundary> b1("pelletinflowboundary");
